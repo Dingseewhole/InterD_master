b28d460fcd5d253db205cf553f3a871c8122001e
Dingseewhole <44332832+Dingseewhole@users.noreply.github.com>
2022-05-24 19:47:56 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -10,7 +10,6 @@ def parse_args():
     parser.add_argument('--type', type=str, default='explicit', help='feedback type. implicit,explicit')
     parser.add_argument('--val_diff', type=str, default='None', help='if it can be different, uniform or bias data to val')
 
-    # parser.add_argument('--teacher_model_args', type=json.loads, default='{"emb_dim": 10, "learning_rate": 0.1, "weight_decay": 10}', help='weight model arguments.')
     parser.add_argument('--exp_name', type=str, default=' ',help='to save best model weight with this name')
     parser.add_argument('--gama', type=float, default='0.17',help='CFF gama')
     parser.add_argument('--gama2', type=float, default='1.25',help='CFF_A gama')
